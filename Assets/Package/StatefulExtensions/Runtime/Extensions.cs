0e4cf12d33e39322ab21a54b60f0bd881dfa21b0
Elliot Pjecha <epjecha@gmail.com>
2025-11-01 13:27:31 -0500

    Bind value observable to another value observable

--- Assets/Package/StatefulExtensions/Runtime/Extensions.cs
+++ Assets/Package/StatefulExtensions/Runtime/Extensions.cs
@@ -24,5 +24,23 @@ namespace Nessle.StatefulExtensions
                 value.Subscribe(x => bindTo.ExecuteSetOrDelay(toSource(x.currentValue)))
             );
         }
+
+        public static void BindValue<TProps, TValue>(this IControl<TProps> control, Func<TProps, ValueObservable<TValue>> accessValue, ValueObservable<TValue> bindTo)
+        {
+            var value = accessValue(control.props);
+            control.AddBinding(
+                bindTo.Subscribe(x => value.From(x.currentValue)),
+                value.Subscribe(x => bindTo.From(x.currentValue))
+            );
+        }
+
+        public static void BindValue<TProps, TValue, TSource>(this IControl<TProps> control, Func<TProps, ValueObservable<TValue>> accessValue, ValueObservable<TSource> bindTo, Func<TValue, TSource> toSource, Func<TSource, TValue> toValue)
+        {
+            var value = accessValue(control.props);
+            control.AddBinding(
+                bindTo.Subscribe(x => value.From(toValue(x.currentValue))),
+                value.Subscribe(x => bindTo.From(toSource(x.currentValue)))
+            );
+        }
     }
 }
