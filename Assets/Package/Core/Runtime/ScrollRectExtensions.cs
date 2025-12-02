6a242b143a33545e987c4abd8f17da50440f0769
Elliot Pjecha <epjecha@gmail.com>
2025-12-02 11:18:31 -0600

    scrollrect content methods

---
+++ Assets/Package/Core/Runtime/ScrollRectExtensions.cs
@@ -0,0 +1,22 @@
+using ObserveThing;
+using static Nessle.UIBuilder;
+
+namespace Nessle
+{
+    public static class ScrollRectExtensions
+    {
+        public static T Content<T>(this T control, IControl content)
+            where T : IControl<ScrollRectProps>
+        {
+            control.props.content.From(content);
+            return control;
+        }
+
+        public static T Content<T>(this T control, IValueObservable<IControl> content)
+            where T : IControl<ScrollRectProps>
+        {
+            control.props.content.From(content);
+            return control;
+        }
+    }
+}
